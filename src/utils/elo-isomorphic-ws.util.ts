b76c10d5d333587edc5a1dcfbbfac6fd766bc709
kyawswarthwin <kyawswar.dev@gmail.com>
2025-10-15 05:15:32 +0630

    fix: critical WebSocket class bug and bump version

--- src/utils/elo-isomorphic-ws.util.ts
+++ src/utils/elo-isomorphic-ws.util.ts
@@ -1,17 +1,30 @@
 import { WebSocket as NodeWebSocket } from 'ws';
 
+const isNode = globalThis.process?.release?.name === 'node';
+
 interface WebSocketInit {
   protocols?: string | string[];
   headers?: HeadersInit;
 }
 
-const isNode = globalThis.process?.release?.name === 'node';
-
-export const WebSocket = (
-  isNode ? NodeWebSocket : globalThis.WebSocket
-) as typeof globalThis.WebSocket & {
+type ELOWebSocketConstructor = typeof globalThis.WebSocket & {
   new (
     url: string | URL,
     protocols?: string | string[] | WebSocketInit,
   ): WebSocket;
 };
+
+class ELOWebSocket extends (isNode ? NodeWebSocket : globalThis.WebSocket) {
+  constructor(
+    url: string | URL,
+    protocols?: string | string[] | WebSocketInit,
+  ) {
+    if (typeof protocols === 'object' && !Array.isArray(protocols)) {
+      super(url, isNode ? protocols : protocols.protocols);
+    } else {
+      super(url, protocols);
+    }
+  }
+}
+
+export const WebSocket = ELOWebSocket as ELOWebSocketConstructor;
